1caf7aa7294050fe5e510be053f9b2e8cbae55d8
drunker121 <87312371+drunker121@users.noreply.github.com>
2023-01-17 11:01:39 +0530

    frontend-home

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import {Routes, Router} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Calci from './Components/Calci';
+import Home from './Components/Home'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      {/* <Calci/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import img1 from '../Images/1.jpg'
+import img2 from '../Images/6.jpg'
+import img3 from '../Images/10.jpg'
+import 'bootstrap/dist/css/bootstrap.css';
+import './Home.css'
+
+
+
+const Home = () => {
+  return (
+    <>
+    {/* <div className='body'>
+        <div className='imgSlider'>
+
+        </div>
+    </div> */}
+    
+    </>
+  )
+}
+
+export default Home
