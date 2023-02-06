375a266730097876c21c81612b8d35c8b6e62ffb
drunker121 <87312371+drunker121@users.noreply.github.com>
2023-02-06 13:10:52 +0530

    Axios

--- client/src/Context/ProductContext.js
+++ client/src/Context/ProductContext.js
@@ -1,8 +1,22 @@
-import  { createContext } from 'react';
+import  { createContext, useEffect } from 'react';
+import axios from 'axios';
 
 const AppContext = createContext();
 
 const AppProvider = ({children}) => {
+
+    const API = "https://drunker121.github.io/MarbleGramAPI/Products.json";
+
+    const getProducts = async (url) =>{
+        const res = await axios.get(url);
+        const products = await res.data;
+        console.log(products);
+    };
+
+    useEffect(() =>{
+        getProducts(API);
+    }, [])
+
     return <AppContext.Provider value="">{children}</AppContext.Provider>
 };
 
